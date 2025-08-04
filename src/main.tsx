71c029f05963d0dc630d8b1d4f041be330b87511
Aurelmax <aurelien@gestionmax.fr>
2025-08-04 15:15:06 +0200

    feat: Intégration d'axe-core pour l'audit d'accessibilité

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Intégration d'axe-core pour l'audit d'accessibilité en mode développement uniquement
+if (process.env.NODE_ENV !== 'production') {
+  import('@axe-core/react').then(axe => {
+    axe.default(React, ReactDOM, 1000, {});
+    console.log('Axe-core est activé pour l\'audit d\'accessibilité');
+  });
+}
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
